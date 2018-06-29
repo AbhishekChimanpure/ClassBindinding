db1ac0420091db8613ff510d42ee6a4a05cec418
Abhishek <ar16chimanpure@gmail.com>
2018-06-29 22:45:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MarvellousClassComponent } from './marvellous-class/marvellous-class.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarvellousClassComponent
   ],
   imports: [
     BrowserModule
